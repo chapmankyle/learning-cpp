b12a5dad06407688172b1a9032b0b26a4441ba2f
Kyle <kyleichapman@gmail.com>
2020-08-07 12:13:52 +0200

    Add main C++ file

---
+++ Tutorial/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "io.h"
+#include "quizzes.h"
+
+/*
+ * Main function.
+ */
+int main() {
+	// when debugging using printing, use std::cerr instead of std::cout
+	// since the latter is buffered
+
+	// chap4quiz3();
+	// chap6quiz1();
+	chap6quiz3();
+
+	return 0;
+}
